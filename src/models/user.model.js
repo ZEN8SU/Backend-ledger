da012c265d50a70d9f8783ff55522eced5b9c318
ZEN8SU <yashraajsrivastava@gmail.com>
2026-09-07 20:56:24 +0530

    make a user model and some changes in another file

---
+++ src/models/user.model.js
@@ -0,0 +1,61 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required for creating a user"],
+      trim: true,
+      lowercase: true,
+      unique: [true, "Email already exist "],
+      match: [
+        /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/,
+        "Please enter a valid email address",
+      ],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required for creating an account"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required for creating an account"],
+      select: false,
+      minlength: [6, " password should contain more than 6 character"],
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateRefreshToken = async function (){
+  return jwt.sign(
+    {
+      _id: this.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateAccessToken = async function (){
+  return jwt.sign(
+    {
+      _id: this.id,
+      email: this.email,
+      name: this.name,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
